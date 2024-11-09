845bc7349c0ef014c74b63695853876d96e09626
Merve Esra Tekcan <esramerve.tekcan@gmail.com>
2024-11-10 02:52:22 +0300

    Filter Service

--- src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e01cbbea07fe01d3bc43dc7e1b8ccf13c4bfb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6cc394fe0516730e61beb2a4a2407c65f326ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
